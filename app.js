4c99275e1aed6a217c271a1edc52d6fda1a7b5ae
LN Mars <lnor.mars@gmail.com>
2016-11-27 13:19:30 -0500

    revised to actually work

--- app.js
+++ app.js
@@ -1,48 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var blogPosts = [];
-//var Sequelize = require('sequelize');
 
 var app = express();
 var port = 3000;
-/*var databaseURL = ('sqlite://db/app.db');
-var sequelize = new Sequelize(databaseURL);*/
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-
-/*var BlogPosts = sequelize.define('BlogPosts', {
-	title: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		unique: true
-	},
-	author: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		unique: false
-	},
-	body: {
-		type: Sequelize.TEXT,
-		allowNull: false,
-		unique: true
-	}
-});
-
-app.get('/blogPosts', function(req, res) {
-	BlogPosts.findAll().then(function(rows) {
-		res.render("blogPosts", {blogPosts: blogPosts});
-	});
-});*/
-
-app.get('/', function(req, res) {
-	var data = {};
-	data.blogPosts = blogPosts;
-	res.render('blogPosts', data);
-});
 
 app.post('/create', function(req, res) {
 	var newPost = {};
@@ -55,12 +22,12 @@ app.post('/create', function(req, res) {
 	blogPosts.push(newPost);
 
 	console.log(blogPosts);
+	res.render('blogPosts', {blogPosts: blogPosts});
 	res.redirect('/');
-	});
+});
 
 app.get('/', function(req, res) {
-	res.render(blogPosts);
-	res.redirect('/');
+	res.render('blogPosts', blogPosts);
 });
 
 app.get('/*', function(req, res) {
